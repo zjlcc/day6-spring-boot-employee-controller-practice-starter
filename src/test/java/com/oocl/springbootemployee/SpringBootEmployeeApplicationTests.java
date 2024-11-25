873b9cff63bc38064098481033e9784e97109a65
ZHENGVI2 <Zhengvi2@oocl.com>
2024-11-25 15:32:45 +0800

    [SpringBootEmployee]test, add should_return_employees_when_get_all_given_employees test

--- src/test/java/com/oocl/springbootemployee/SpringBootEmployeeApplicationTests.java
+++ src/test/java/com/oocl/springbootemployee/SpringBootEmployeeApplicationTests.java
@@ -1,13 +1,48 @@
 package com.oocl.springbootemployee;
 
+import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringBootEmployeeApplicationTests {
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private MockMvc mockMvc;
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    void should_return_employees_when_get_all_given_employees() throws Exception {
+        //Given
+        List<Employee> employees = employeeRepository.getAll();
+
+        //When
+
+        //Then
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(employees.get(0).getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value(employees.get(1).getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].name").value(employees.get(2).getName()));
+    }
+
 
 }
